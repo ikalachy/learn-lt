22f38973cb0186ea9bb8d4ffed4d155bc62bbf7b
Ivan_Kalachyev <baton4ik@gmail.com>
2025-03-14 19:46:45 +0200

    fix: update initialization data in StoreContext to correctly reference Telegram web app data

--- src/contexts/StoreContext.jsx
+++ src/contexts/StoreContext.jsx
@@ -73,7 +73,7 @@ export function StoreProvider({ children }) {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({ 
-            initData: params.initData,
+            initData: params.tgWebAppData,
           }),
         });
 
