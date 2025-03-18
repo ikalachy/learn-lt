32ab2398ec74114eb1b58673b716be67a37a7095
Ivan_Kalachyev <baton4ik@gmail.com>
2025-03-18 13:01:08 +0200

    feat: enhance payload creation for TON payments in PaymentButton component

--- src/components/PaymentButton.jsx
+++ src/components/PaymentButton.jsx
@@ -4,14 +4,14 @@ import { useStore } from "@/contexts/StoreContext";
 import { CHAIN, TonConnectButton, useTonConnectUI } from "@/tonconnect";
 import { toNano } from "ton-core";
 import { useState } from "react";
+import { beginCell } from "@ton/ton";
 
 const BOT_USERNAME =
   process.env.NEXT_PUBLIC_TELEGRAM_BOT_USERNAME || "LearnLTBot";
 const TON_NETWORK = process.env.TON_NETWORK || "testnet";
 
 const boc =
-  // "te6cckECGQEAA2YAA+eIAU9LXt62pRYnQfUDmoapUFcS5+IItGZnYuQwxohN7iIYEY5tLO3P///iP////+AAAAATepimN8bHZTA6el+C9hauQhRrycbyLO8jAlFW16vHstM4BqyGuDhnLsmJFGL6sQABMVAprp/JreJN/YImy/awLAEVFgEU/wD0pBP0vPLICwICASADDgIBSAQFAtzQINdJwSCRW49jINcLHyCCEGV4dG69IYIQc2ludL2wkl8D4IIQZXh0brqOtIAg1yEB0HTXIfpAMPpE+Cj6RDBYvZFb4O1E0IEBQdch9AWDB/QOb6ExkTDhgEDXIXB/2zzgMSDXSYECgLmRMOBw4hEQAgEgBg0CASAHCgIBbggJABmtznaiaEAg65Drhf/AABmvHfaiaEAQ65DrhY/AAgFICwwAF7Ml+1E0HHXIdcLH4AARsmL7UTQ1woAgABm+Xw9qJoQICg65D6AsAQLyDwEeINcLH4IQc2lnbrry4Ip/EAHmjvDtou37IYMI1yICgwjXIyCAINch0x/TH9Mf7UTQ0gDTHyDTH9P/1woACvkBQMz5EJoolF8K2zHh8sCH3wKzUAew8tCEUSW68uCFUDa68uCG+CO78tCIIpL4AN4BpH/IygDLHwHPFsntVCCS+A/ecNs82BED9u2i7fsC9AQhbpJsIY5MAiHXOTBwlCHHALOOLQHXKCB2HkNsINdJwAjy4JMg10rAAvLgkyDXHQbHEsIAUjCw8tCJ10zXOTABpOhsEoQHu/Lgk9dKwADy4JPtVeLSAAHAAJFb4OvXLAgUIJFwlgHXLAgcEuJSELHjDyDXShITFACWAfpAAfpE+Cj6RDBYuvLgke1E0IEBQdcY9AUEnX/IygBABIMH9FPy4IuOFAODB/Rb8uCMItcKACFuAbOw8tCQ4shQA88WEvQAye1UAHIw1ywIJI4tIfLgktIA7UTQ0gBRE7ry0I9UUDCRMZwBgQFA1yHXCgDy4I7iyMoAWM8Wye1Uk/LAjeIAEJNb2zHh10zQAFGAAAAAP///iOqpVFeyWOS2ZhaNXg1sW9FkZJ1GVJewbKlv+6uDqs/woAIKDsPIbQMXGAAAAGhCACQCEi2YcPh1+5AOsZKerosRRjTPt5Q2SNiaQT1S3CZzIC+vCAAAAAAAAAAAAAAAAAAAS34S9Q==";
-  "te6cckEBBAEAtwAB5YgBT0te3ralFidB9QOahqlQVxLn4gi0Zmdi5DDGiE3uIhgDm0s7c///+Is+w33IAAAAJNiaH6CAXz8FPUvFDNfOula/200aDzwWU4vwej3ZrC9eTX+fR7PQtwAA33RBKUvf0khgDRWgCj9YddAAYQCvJAsBAgoOw8htAwIDAAAAaEIAJAISLZhw+HX7kA6xkp6uixFGNM+3lDZI2JpBPVLcJnMgL68IAAAAAAAAAAAAAAAAAAAk+lG8"
+  "te6cckEBBAEAtwAB5YgBT0te3ralFidB9QOahqlQVxLn4gi0Zmdi5DDGiE3uIhgDm0s7c///+Is+ynYAAAAALB7kekrwgvhnwDecD2QXye+vwTStN4fecBhrkmbRDxTocefh0cJu/PLQSe0pd3S3SSxfoaz9Vu6I7tePvk5diBMBAgoOw8htAwIDAAAAaEIAJAISLZhw+HX7kA6xkp6uixFGNM+3lDZI2JpBPVLcJnMgL68IAAAAAAAAAAAAAAAAAADAD3n7";
 
 const TON_PRICE = 0.1; // Price in TON
 
@@ -25,6 +25,10 @@ export default function PaymentButton() {
     setIsLoading(true);
     setStatus(null);
     try {
+      const payload = beginCell()
+        .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
+        .storeStringTail(`premium_${user?.telegramId}`) // write our text comment
+        .endCell();
       const transactionData = {
         validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes
         network: TON_NETWORK === "mainnet" ? CHAIN.MAINNET : CHAIN.TESTNET,
@@ -32,7 +36,7 @@ export default function PaymentButton() {
           {
             address: process.env.NEXT_PUBLIC_OWNER_WALLET,
             amount: toNano(TON_PRICE).toString(),
-            // payload: `premium_${user?.telegramId}`.toString("base64") // payload with comment in body ,
+            payload: payload.toBoc().toString("base64"), // payload with comment in body ,
           },
         ],
       };
